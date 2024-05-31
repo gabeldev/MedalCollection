34d02db06246c5e45b1526cc5642cd1b0468ec62
felipe-rodriguesz <feliperodrigues.2004@alunos.utfpr.edu.br>
2024-05-31 18:25:05 -0300

    changes binario.c

--- binario.c
+++ binario.c
@@ -1,41 +1,49 @@
-#include "cadastro.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "binario.h"
 
-// Função para salvar os dados das medalhas em um arquivo binário
-void salvarMedalhas (char *nomeArquivo, Medalha *medalhas, int quantidade) {
+// Função para salvar os dados em um arquivo binário
+void salvar_binario(Medalhas *medalha, int total_medalhas) {
 
-    FILE *arquivo = fopen (nomeArquivo, "wb");
-    if (arquivo == NULL) {
-        perror ("Erro ao abrir o arquivo para escrita");
-        exit(EXIT_FAILURE);
+    FILE *bin_file;
+    char *filename = "medalhas.bin";
+
+    bin_file = fopen(filename, "wb");
+    if (bin_file == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+
+    fwrite(&total_medalhas, sizeof(int), 1, bin_file);
+    fwrite(medalha, sizeof(Medalhas), total_medalhas, bin_file);
+
+    fclose(bin_file);
+}//salvar_binario
+
+// Função para carregar os dados do arquivo binário
+Medalhas* carregar_binario(int *total_medalhas) {
+    FILE *bin_file;
+    char *filename = "medalhas.bin";
+    Medalhas *medalha;
+
+    bin_file = fopen(filename, "rb");
+    if (bin_file == NULL) {
+        perror("Erro ao abrir o arquivo");
+        *total_medalhas = 0;
+        return NULL;
     }//if
 
-    fwrite (&quantidade, sizeof(int), 1, arquivo);
-    fwrite (medalhas, sizeof(Medalha), quantidade, arquivo);
+    fread(total_medalhas, sizeof(int), 1, bin_file);
 
-    fclose (arquivo);
-
-}//salvarMedalhas
-
-// Função para carregar os dados das medalhas de um arquivo binário
-Medalha* carregarMedalhas (char *nomeArquivo, int *quantidade) {
-
-    FILE *arquivo = fopen(nomeArquivo, "rb");
-    if (arquivo == NULL) {
-        perror("Erro ao abrir o arquivo para leitura");
-        exit(EXIT_FAILURE);
-    }//if
-
-    fread (quantidade, sizeof(int), 1, arquivo);
-    Medalha *medalhas = (Medalha*) malloc(sizeof(Medalha) * (*quantidade));
-    if (medalhas == NULL) {
+    medalha = (Medalhas*)malloc(*total_medalhas * sizeof(Medalhas));
+    if (medalha == NULL) {
         perror("Erro ao alocar memória");
+        fclose(bin_file);
         exit(EXIT_FAILURE);
     }//if
 
-    fread (medalhas, sizeof(Medalha), *quantidade, arquivo);
+    fread(medalha, sizeof(Medalhas), *total_medalhas, bin_file);
 
-    fclose (arquivo);
-
-    return medalhas;
-}//carregarMedalhas
+    fclose(bin_file);
+    return medalha;
+}//carregar_binario
